2544e0570708fb4a370e3fcd46f602be45b505c6
sonny-weir <github@macanmhaoir.net>
2023-10-28 23:30:46 +0200

    update deploy.yml

--- src/components/Privacy.vue
+++ src/components/Privacy.vue
@@ -71,7 +71,6 @@
     padding: 15px 20px;
     background-color: #FFFFFF;
     border-radius: 5px;
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
     line-height: 1.6;
     color: #333;
     margin-bottom: 20px;
