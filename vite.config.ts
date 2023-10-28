f73d5a090731c8376765895fccebeb554a590c70
sonny-weir <github@macanmhaoir.net>
2023-10-28 18:03:20 +0200

    changed config 2

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './'
+  resolve:{
+    extensions: ['.js','.jsx','.mjs','.ts', '.tsx','.vue', '.json']
+
+  },
+  base: './dist'
 })
