865727a1f6d3a10db472fe869ea095fdb7a6a66b
sonny-weir <github@macanmhaoir.net>
2023-10-30 14:22:53 +0100

    broadened description somewhat

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,7 +3,8 @@
 </script>
 
 <template>
-  <p>Blaumac Consulting is a company specializing in information technology services. We also manage several coworking spaces to support professionals in their work.</p>
+  <p>Blaumac Consulting is a company specializing in information technology services. In particular we provide statistical programming services and are experts in SAS, R and Python.</p>
+  <p>We also manage several coworking spaces to support professionals in their work.</p>
 </template>
 
 <style scoped>
